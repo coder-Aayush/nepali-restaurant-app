228bafcd1a550bcee951a0473060ca66ad45b6ed
Bipin Sainju Shrestha <bipinsainjushrestha@192.168.1.167>
2026-04-15 07:50:08 +0545

    fixing

--- test/nepali_restaurant_app_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:nepali_restaurant_app/nepali_restaurant_app.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
